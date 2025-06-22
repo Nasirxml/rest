409087b3def087b198a1a0eb7a06854f3514626c
Medusaa <am1tahunprem@gmail.com>
2025-06-23 00:20:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@
                 
                 const responseData = {
                     statusCode: statusCode,
-                    creator: '@rynxzyy',
+                    creator: '@Nasirxml',
                     ...data
                 };
                 
